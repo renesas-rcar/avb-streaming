a573d90f0467bfe1e2fc06fc0790fd71d4840488
Masaru Nagai <masaru.nagai.vx@renesas.com>
2017-06-08 17:02:07 +0900

    ravb_streaming: update version to v2.9.0
    Signed-off-by: Masaru Nagai <masaru.nagai.vx@renesas.com>

--- ravb_streaming_version.h
+++ ravb_streaming_version.h
@@ -63,6 +63,6 @@
 #define __RAVB_STREAMING_VERSION_H__
 
 #define DRIVER "ravb-streaming"
-#define DRIVER_VERSION "R-Car v2.7.0"
+#define DRIVER_VERSION "R-Car v2.9.0"
 
 #endif	/* __RAVB_STREAMING_VERSION_H__ */
